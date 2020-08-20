5aadf763a9377b80041e789473eb32d7059e495f
Jacob Quisenberry <jacob.quisenberry@consilio.com>
2020-08-20 16:35:13 -0700

    Updated local host to 127.0.0.1 from 0.0.0.0, which works poorly under Windows

--- graphserver/app.py
+++ graphserver/app.py
@@ -40,4 +40,5 @@ if __name__ == "__main__":
 
 
     app = create_app(bolt_uri=bolt_uri)
-    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=80)  # Original
+    app.run(host='127.0.0.1', port=5556)  # Windows 10
